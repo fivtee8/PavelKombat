d0c1272bc99a67f2d66d6b9c89b2ccc8d6320f1d
stranger-ips <mango.res@mail.ru>
2024-07-18 20:02:43 +0300

    minor bug fixes

--- bot.py
+++ bot.py
@@ -47,9 +47,8 @@ async def start_handler(message: types.Message):
     set_await = requests.get(f'https://fond-pangolin-lately.ngrok-free.app/botapi/set_await_query_id/{message.from_user.id}/{os.getenv("botkey")}')
 
     if set_await.json()['code'] == '0':
-        sent = await message.answer("Начни тапать Павла Сергеевича! \n Данное сообщение будет удалено через 15 секунд для предотвращения аттак.", reply_markup=keyboard)
+        sent = await message.answer("Начни тапать Павла Сергеевича! \n Данное сообщение будет удалено через 15 секунд для предотвращения атак.", reply_markup=keyboard)
         await asyncio.sleep(15)
-        await bot.delete_message(sent.chat.id, message.message_id)
         await bot.delete_message(sent.chat.id, sent.message_id)
         requests.get(f'https://fond-pangolin-lately.ngrok-free.app/botapi/unawait_query/{message.from_user.id}')
     else:
