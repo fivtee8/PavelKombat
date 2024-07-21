6481edfc8a96bf9918832b22c4cf529893276b48
fivtee8 <fivtee8@gmail.com>
2024-07-21 22:18:20 +0300

    Added ngrok wait loop

--- test.sh
+++ test.sh
@@ -2,6 +2,9 @@
 fuser database.db 2>/dev/null | xargs kill -9
 ./startserver.sh
 python3 tests/clear_db.py
+while ! nc -z localhost 4040; do
+  sleep 1/5 # wait Ngrok to be available
+done
 python3 -m unittest discover tests --verbose 2> logs/test_out.txt
 ./stopserver.sh
 tail -4 logs/test_out.txt
