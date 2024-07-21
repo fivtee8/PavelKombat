8b97070f0964eb733f2352dfc2eec1ac1dfa8b76
fivtee8 <fivtee8@gmail.com>
2024-07-21 22:00:13 +0300

    Modified test.sh

--- test.sh
+++ test.sh
@@ -2,6 +2,7 @@
 python3 tests/clear_db.py
 fuser database.db 2>/dev/null | xargs kill -9
 ./startserver.sh
+curl http://127.0.0.1:5005/ > /dev/null 2> /dev/null
 python3 -m unittest discover tests --verbose 2> logs/test_out.txt
 ./stopserver.sh
 tail -4 logs/test_out.txt
